936f62b7eff3bcb260a553522c7ad6b85864427e
IzuruSakamaki <50161133+IzuruSakamaki@users.noreply.github.com>
2019-10-16 16:38:29 +0700

    Update um.php

--- um.php
+++ um.php
@@ -29,6 +29,11 @@
 			<header class="site-navbar site-navbar-target" role="banner">
 				<div class="container">
 					<div class="row align-items-center position-relative">
+						<div class="col-3 ">
+							<div class="site-logo">
+								<a href="index.html">SmartFX</a>
+							</div>
+						</div>
 						<div class="col-9  text-right">
 							<span class="d-inline-block d-lg-none"><a href="#" class="text-white site-menu-toggle js-menu-toggle py-5 text-white"><span class="icon-menu h3 text-white"></span></a></span>
 						</div>
