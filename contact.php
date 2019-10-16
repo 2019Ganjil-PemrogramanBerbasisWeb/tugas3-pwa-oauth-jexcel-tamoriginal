6f6809e2158382f7d12b7116ff81eefc7b0ba4cc
Anggada Putra <47882546+anggadaputra11319@users.noreply.github.com>
2019-10-16 23:28:57 +0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -78,6 +78,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                 <li class="active"><a href="contact.php" class="nav-link">Contact</a></li>
                 <li><a href="login.php" class="nav-link"><?php echo htmlspecialchars($_SESSION["username"]); ?></a></li>
                 <li><a href="logout.php" class="nav-link">Sign Out</a></li>
+                <li><a href="reset-password.php" class="nav-link">Reset Password</a></li>
               </ul>
             </nav>
           </div>
