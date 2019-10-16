8fa99cd4d50c77e962e87376c2617c0f8751f9c8
Anggada Putra <47882546+anggadaputra11319@users.noreply.github.com>
2019-10-16 23:35:26 +0700

    Update reset-password.php

--- reset-password.php
+++ reset-password.php
@@ -90,17 +90,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 
 <body>
+    <center>
     <div class="wrapper">
         <h2>Reset Password</h2>
-        <p>Please fill out this form to reset your password.</p>
+        <p style="float: left;">Please fill out this form to reset your password.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group <?php echo (!empty($new_password_err)) ? 'has-error' : ''; ?>">
-                <label>New Password</label>
+                <label style="float: left;">New Password</label>
                 <input type="password" name="new_password" class="form-control" value="<?php echo $new_password; ?>">
-                <span class="help-block"><?php echo $new_password_err; ?></span>
+                <span class="help-block"><?phstyle="float:left;"p echo $new_password_err; ?></span>
             </div>
             <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
-                <label>Confirm Password</label>
+                <label style="float: left;">Confirm Password</label>
                 <input type="password" name="confirm_password" class="form-control">
                 <span class="help-block"><?php echo $confirm_password_err; ?></span>
             </div>
@@ -110,6 +111,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </div>
         </form>
     </div>
+    </center>
 </body>
 
 </html>
