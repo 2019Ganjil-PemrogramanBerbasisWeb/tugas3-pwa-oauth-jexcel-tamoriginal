73435af8c938193a46590b6fa8eb0feb37db958b
Anggada Putra <47882546+anggadaputra11319@users.noreply.github.com>
2019-10-16 16:53:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -97,7 +97,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 
               <div class="d-flex media-38289 justify-content-around mb-5">
               </div>
-              <p><a href="um.php" class="btn btn-primary text-white px-4 py-3">Learn More</a></p>
+              <p><a href="service.php" class="btn btn-primary text-white px-4 py-3">Learn More</a></p>
             </div>
           </div>
         </div>
