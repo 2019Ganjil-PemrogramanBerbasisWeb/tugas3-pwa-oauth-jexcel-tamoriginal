269640f859ee96850f2eed61a58b097238c4b462
Christian Andrew <christianandrew7449@gmail.com>
2019-10-20 11:19:26 +0700

    Update reset-password.php

--- reset-password.php
+++ reset-password.php
@@ -60,11 +60,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 echo "Oops! Something went wrong. Please try again later.";
             }
         }
-
         // Close statement
         mysqli_stmt_close($stmt);
     }
-
     // Close connection
     mysqli_close($link);
 }
