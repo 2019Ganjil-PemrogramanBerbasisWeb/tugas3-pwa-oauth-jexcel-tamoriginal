14a4f663c1c12b37f11157dc1ae524860e612dd4
Christian Andrew <christianandrew7449@gmail.com>
2019-10-20 10:13:48 +0700

    Update service.php

--- service.php
+++ service.php
@@ -263,7 +263,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
               </form>
             </div>
             <div class="col-md-3 ml-auto">
-              <h3 class="text-white h5 mb-3">Subscribe</h3>
+              <h3 class="text-white h5 mb-3">More Information</h3>
               <ul class="list-unstyled menu-arrow">
                 <li><a href="um.php">About Us</a></li>
                 <li><a href="um.php">Testimonials</a></li>
